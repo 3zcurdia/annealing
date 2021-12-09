a09dcf0199aea4ce0fb840ffda530142b8758e36
Chris Bloom <chrisbloom7@github.com>
2021-12-09 09:54:25 -0500

    Updating rubocop config and linting files

--- test/annealing/simulator_test.rb
+++ test/annealing/simulator_test.rb
@@ -16,14 +16,14 @@ describe Annealing::Simulator do
     ]
   end
   let(:default_energy_calculator) do
-    -> (locations) do
+    lambda do |locations|
       locations.each_cons(2).sum do |location1, location2|
         location1.distance(location2)
       end
     end
   end
   let(:default_state_change) do
-    -> (locations) do
+    lambda do |locations|
       size = locations.size
       swapped = locations.dup
       idx_a = rand(size)
@@ -44,21 +44,21 @@ describe Annealing::Simulator do
     Annealing.configuration.reset
   end
 
-  it "uses the global energy calculator and state change method" do
+  it 'uses the global energy calculator and state change method' do
     initial_energy = default_energy_calculator.call(locations)
     assert_equal 46, initial_energy
     simulation = simulator.run(locations)
     assert_operator simulation.energy, :<, initial_energy
   end
 
-  it "can override the global energy calculator and state change method" do
+  it 'can override the global energy calculator and state change method' do
     initial_state = rand * 16
-    local_energy_calculator = -> (state) { state * state - 16 }
-    local_state_change = -> (state) { state + (rand - 0.5) }
+    local_energy_calculator = ->(state) { (state**2) - 16 }
+    local_state_change = ->(state) { state + (rand - 0.5) }
     initial_energy = local_energy_calculator.call(initial_state)
     simulation = simulator.run(initial_state,
-                                energy_calculator: local_energy_calculator,
-                                state_change: local_state_change)
+                               energy_calculator: local_energy_calculator,
+                               state_change: local_state_change)
     assert_operator simulation.energy, :<, initial_energy
   end
 end
